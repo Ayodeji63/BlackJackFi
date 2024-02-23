e23bfd56afb11b100227f36e003ec7400eb089b5
Ayodeji <olusanyasamson633@gmail.com>
2024-02-23 19:23:54 +0100

    building casinofi token contract and staking contract

---
+++ contracts/src/CasinoFiToken.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CasinoFiToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 3e8 * 10**18; // 21 million tokens
+    uint256 public constant EARLY_ACCESS_SUPPLY = (5 * MAX_SUPPLY) / 100; // 5% for early users
+    uint256 public constant TREASURY_SUPPLY = (55 * MAX_SUPPLY) / 100; // 55% for treasury
+    uint256 public constant DEVELOPER_REWARDS_SUPPLY = (4 * MAX_SUPPLY) / 100; // 4% for developers
+    uint256 public constant STAKING_REWARDS_SUPPLY = (1 * MAX_SUPPLY) / 100; // 1% for staking rewards
+    uint256 public constant BUYBACK_AND_BURN_SUPPLY = (1 * MAX_SUPPLY) / 100; // 1% for buyback and burn
+    uint256 public constant EARLY_ACCESS_AMOUNT = 100;
+    address public immutable i_owner;
+
+
+    bool public earlyAccessEnabled = true;
+
+    constructor(address owner) ERC20("CasinoFiToken", "CFI") Ownable(owner) {
+        i_owner = owner;
+          // Mint initial supplies
+        _mint(owner, EARLY_ACCESS_SUPPLY);
+        _mint(address(this), TREASURY_SUPPLY);
+        _mint(owner, DEVELOPER_REWARDS_SUPPLY);
+
+        // Mint additional supplies
+        _mint(address(this), STAKING_REWARDS_SUPPLY);
+        _mint(address(this), BUYBACK_AND_BURN_SUPPLY);
+    }
+
+    function mintForEarlyUsers() external {
+        require(earlyAccessEnabled, "Early access has ended");
+
+        uint256 remainingSupply =  balanceOf(owner()) - DEVELOPER_REWARDS_SUPPLY;
+        require(EARLY_ACCESS_AMOUNT <= remainingSupply, "Exceeds available allocation");
+
+        _mint(msg.sender, EARLY_ACCESS_AMOUNT);
+    }
+
+    function endEarlyAccess() external onlyOwner {
+        earlyAccessEnabled = false;
+    }
+
+    // Other functions...
+
+    // In case you want to transfer tokens allocated for developers
+    function transferDevelopersAllocation(address to, uint256 amount) external onlyOwner {
+        require(amount <= DEVELOPER_REWARDS_SUPPLY, "Exceeds developers allocation");
+        _transfer(owner(), to, amount);
+    }
+}
---
+++ contracts/test/CasinoFiToken.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {CasinoFiToken} from "../src/CasinoFiToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CasinoFiTokenTest is Test {
+
+    CasinoFiToken public casinoFiToken;
+    address public OWNER = makeAddr("owner");
+    address public PLAYER1 = makeAddr("player1");
+    uint256 public EARLY_ACCESS_REWARD = 100;
+
+
+    function setUp () public {
+        casinoFiToken = new CasinoFiToken(OWNER);
+    }
+
+    function test_owner_balance_is_greater_than_zero() public view {
+      uint256 ownerBalance =  IERC20(address(casinoFiToken)).balanceOf(OWNER);
+      uint256 totalTokenOwnerHolding = casinoFiToken.DEVELOPER_REWARDS_SUPPLY() + casinoFiToken.EARLY_ACCESS_SUPPLY();
+      assert(ownerBalance == totalTokenOwnerHolding);
+    }
+
+    function test_tokens_contract_holding_is_correct() public view {
+        uint256 contractBalance = IERC20(address(casinoFiToken)).balanceOf(address(casinoFiToken));
+        uint256 expectTotalToken = casinoFiToken.STAKING_REWARDS_SUPPLY() + casinoFiToken.BUYBACK_AND_BURN_SUPPLY();
+        assert(contractBalance == expectTotalToken);
+    }
+
+    function test_can_mint_early_access_token() public  {
+        vm.deal(PLAYER1, 1 ether);
+        vm.prank(PLAYER1);
+        casinoFiToken.mintForEarlyUsers();
+        uint256 playertokenBalance = IERC20(address(casinoFiToken)).balanceOf(PLAYER1);
+        assert(playertokenBalance == EARLY_ACCESS_REWARD);
+    }
+
+}
